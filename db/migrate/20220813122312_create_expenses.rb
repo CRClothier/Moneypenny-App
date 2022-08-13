ca8c39fa22f4a605dbb6508a8e91bc021e32de3a
Chris Clothier <c.r.clothier@gmail.com>
2022-08-14 00:13:43 +0100

    Update migrate

--- db/migrate/20220813122312_create_expenses.rb
+++ db/migrate/20220813122312_create_expenses.rb
@@ -2,7 +2,7 @@ class CreateExpenses < ActiveRecord::Migration[7.0]
   def change
     create_table :expenses do |t|
       t.string :name
-      t.float :amount
+      t.float :amount, default: 0.00
 
       t.timestamps
     end
